d6ca63ae7a1896ff4e94256170ad73d26b864583
Kara <contact@karajensen.net>
2015-07-30 14:10:09 -0600

    Create footer.ts

---
+++ src/templates/footer.ts
@@ -0,0 +1,12 @@
+import {Component, View, bootstrap}  from 'angular2/angular2';
+@Component({
+    selector: 'footer'
+})
+
+@View({
+    templateUrl: 'components/templates/footer.html'
+})
+
+class Footer { }
+
+bootstrap(Footer);
